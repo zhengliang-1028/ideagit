dad7475c73a27b611c5aafe70b11b80f8bb16f97
zhengliang <3337270442@qq.com>
2020-04-01 00:26:39 +0800

    把本地工程添加到github

---
+++ idea-git-test/src/com/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+    }
+}
